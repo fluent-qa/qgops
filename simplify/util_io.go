2d15517f896ae3028abdf3b5db2bc141e86840a0
DamnPat <patrickwuke@163.com>
2025-04-27 14:02:06 +0800

    udpate to latest codes

---
+++ simplify/util_io.go
@@ -0,0 +1,8 @@
+package simplify
+
+//A Lodash-style Go library based on Go 1.18+ Generics (map, filter, contains, find...)
+//
+//pkg.go.dev/github.com/samber/lo
+//slog
+
+names := lo.Uniq([]string{"Samuel", "John", "Samuel"})
